0c67b4ec57a04c39ec397e91cb7f6e1b7cb56a12
thiago <christiann.teixeira@gmail.com>
2026-01-19 13:29:13 -0300

    first commit

---
+++ QuestionService.java
@@ -0,0 +1,19 @@
+public class QuestionService {
+
+    
+Question[] questions = new Question[5];
+
+public QuestionService(){
+    questions[0] = new Question(1, "what", "java", "c++", "php", "javascript", "java");
+    questions[1] = new Question(1, "what", "java", "c++", "php", "javascript", "java");
+    questions[2] = new Question(1, "what", "java", "c++", "php", "javascript", "java");
+    questions[3] = new Question(1, "what", "java", "c++", "php", "javascript", "java");
+    questions[4] = new Question(1, "what", "java", "c++", "php", "javascript", "java");
+}
+
+public void displayQuestion()
+{
+
+}
+
+}
